906763a5602a2379f40bc6204c725da1e4443a6b
Prakhar Aggarwal <cs5190441@cse.iitd.ac.in>
2021-06-06 01:46:00 +0530

    BIGNUM Package (Yet to add Group Generator)

--- Code/ECC/FiniteFieldElement.hpp
+++ Code/ECC/FiniteFieldElement.hpp
@@ -3,46 +3,82 @@
 #pragma once
 #include <iostream>
 #include <ostream>
+#include <string>
 #include <openssl/crypto.h>
 #include <openssl/bn.h>
+#include "constants.hpp"
+
+// Use BN_value_one() for 1
+// BN_cmp(BIGNUM* a, BIGNUM* b) returns 0 if a == b
+
+// using namespace Constants;
+
+// ostream& operator<<(ostream& os, BIGNUM* g);
+// ostream& operator<<(ostream& os, const BIGNUM* g);
 
 namespace Crypto
 {
     namespace helper
     {
-        int ExtendedGCD(int a, int b, int &x, int &y)
+        // Constants::constants();
+
+        BIGNUM* ExtendedGCD(BIGNUM* a, BIGNUM* b, BIGNUM* &x, BIGNUM* &y)
         {
             // Reference -> https://www.geeksforgeeks.org/euclidean-algorithms-basic-and-extended/
             // Base Case
-            if (a == 0)
-            {
-                x = 0;
-                y = 1;
+            if( BN_cmp(a, BN_ZERO) == 0)
+            {            
+                x = BN_new();
+                y = BN_dup(BN_ONE);
                 return b;
             }
 
-            int x1, y1; // To store results of recursive call
-            int gcd = ExtendedGCD(b%a, a, x1, y1);
+            BIGNUM* x1;
+            BIGNUM* y1;        // To store results of recursive call
+            BIGNUM* b_mod_a;
+            BN_CTX *ctx = BN_CTX_new();
+
+            b_mod_a = BN_dup(a);
+            x1 = BN_dup(a);
+            y1 = BN_dup(b);
+
+            BN_mod(b_mod_a, b, a, ctx);
+            BIGNUM* gcd = ExtendedGCD(b_mod_a, a, x1, y1);
 
             // Update x and y using results of
             // recursive call
-            x = y1 - (b/a) * x1;
-            y = x1;
+            BN_div(x, NULL, b, a, ctx);
+            BN_mul(x, x, x1, ctx);
+            BN_sub(x, y1, x);
+            BN_add(y, x1, BN_ZERO);
+            // x = y1 - (b/a) * x1;
+            // y = x1;
 
             return gcd;
         }
 
-        int MultiplicativeInverse(int a, int m)  // a*b == 1 (mod m) Need to calculate b
+        BIGNUM* MultiplicativeInverse(BIGNUM* a, BIGNUM* m)
         {
-            a = a % m;
-            int x,y;
-            int g = ExtendedGCD(a, m, x, y);
-            if( g!= 1)
+            // a*b == 1 (mod m) Need to calculate b
+            BN_CTX *ctx = BN_CTX_new();
+            // a = a % m;
+            BN_mod(a, a, m, ctx);
+            BIGNUM* x = BN_new();
+            BIGNUM* y = BN_new();
+            
+            BIGNUM* g = ExtendedGCD(a, m, x, y);
+
+            if( BN_cmp(g, BN_ONE) != 0) // Returns Zero if a == b
             {
-                std::cout << "ERROR: For MulInverse, numbers should ne Coprime\n";                
-                return -1;
+                std::cout << "ERROR: For MulInverse, numbers should be Coprime\n";
+                BN_set_word(x, -1);
+                return x;
             }
-            x = x % m;
+            BN_mod(x, x, m, ctx);
+            BN_add(x, x, m);
+            BN_mod(x, x, m, ctx);
+            // x = ((x % m) + m )%m;
+
             return x;
         }
     }
@@ -56,113 +92,337 @@ namespace Crypto
         The template argument T is the order of the field
     */
     
-    template<int P>
+    // template<BIGNUM* PRIME>
     class   FiniteFieldElement
     {
-        int     i_;
+        BIGNUM*     i_ = NULL;
         
-        void    assign(int i)
+        void    assign(BIGNUM* i)
         {
-            i_ = ((i%P) + P)%P;
+            i = BN_dup(i);
+            if(i_ == NULL)i_ = BN_new();
+            BN_CTX *ctx = BN_CTX_new();
+            BN_mod(i, i, PRIME, ctx);
+            BN_add(i, i, PRIME);
+            BN_mod(i_, i, PRIME, ctx);
+            // i_ = ((i%PRIME) + PRIME)%PRIME;
         }
         
         public:
             // ctor
             FiniteFieldElement()
-                : i_(0)
+                : i_(BN_new())
             {}
             // ctor
-            explicit FiniteFieldElement(int i)
+            explicit FiniteFieldElement(BIGNUM* i)
             {
                 assign(i);
             }
+            explicit FiniteFieldElement(int i)
+            {
+                BIGNUM* tmp = BN_new();
+                BN_set_word(tmp, i);
+                assign(tmp);
+            }
             // copy ctor
-            FiniteFieldElement(const FiniteFieldElement<P>& rhs) 
+            FiniteFieldElement(const FiniteFieldElement& rhs) 
                 : i_(rhs.i_)               
             {}
             
             // access "raw" integer
-            int i() const { return i_; }                
+            BIGNUM* i() const { return i_; }
             // negate
             FiniteFieldElement  operator-() const
             {
-                return FiniteFieldElement(-i_);
-            }                                
-            // assign from integer
-            FiniteFieldElement& operator=(int i)
+                BIGNUM* tmp = BN_new();
+                BN_sub(tmp, BN_ZERO, i_);
+                return FiniteFieldElement(tmp);
+            }                    
+
+            // assign from field element
+            FiniteFieldElement& operator=(const FiniteFieldElement& rhs)
+            {
+                BIGNUM* tmp = BN_dup(rhs.i_);
+                i_ = tmp;
+                return *this;
+            }            
+            // assign from Bignum
+            FiniteFieldElement& operator=(BIGNUM* i)
             {
                 assign(i);
                 return *this;
             }
-            // assign from field element
-            FiniteFieldElement<P>& operator=(const FiniteFieldElement<P>& rhs)
+            // assign from int
+            FiniteFieldElement& operator=(int i)
             {
-                i_ = rhs.i_;
+                BIGNUM* tmp = BN_new();
+                BN_set_word(tmp, i);
+                assign(tmp);
                 return *this;
             }
-            // *=
-            FiniteFieldElement<P>& operator*=(const FiniteFieldElement<P>& rhs)
+            
+
+            // *= Field
+            FiniteFieldElement& operator*=(const FiniteFieldElement& rhs)
             {
-                i_ = (i_*rhs.i_) % P;
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, i_, rhs.i_, ctx);
+                BN_mod(i_, tmp, PRIME, ctx);
+                // i_ = (i_*rhs.i_) % PRIME;
                 return *this;           
             }
-            // ==
-            friend bool    operator==(const FiniteFieldElement<P>& lhs, const FiniteFieldElement<P>& rhs)
+            // *= Bignum
+            FiniteFieldElement& operator*=(BIGNUM* rhs)
             {
-                return (lhs.i_ == rhs.i_);
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, i_, rhs, ctx);
+                BN_mod(i_, tmp, PRIME, ctx);
+                return *this;           
+            }
+            // *= int
+            FiniteFieldElement& operator*=(int rhs)
+            {
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BIGNUM* tmp2 = BN_new();
+                BN_set_word(tmp2, rhs); 
+                BN_mul(tmp, i_, tmp2, ctx);
+                BN_mod(i_, tmp, PRIME, ctx);
+                // i_ = (i_*rhs.i_) % PRIME;
+                return *this;           
+            }
+
+            // == Field
+            friend bool    operator==(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
+            {
+                if(BN_cmp(lhs.i_, rhs.i_) == 0)return true;
+                return false;
+                // return (lhs.i_ == rhs.i_);
+            }
+            // == BIGNUM
+            friend bool    operator==(const FiniteFieldElement& lhs, BIGNUM* rhs)
+            {
+                if( BN_cmp(lhs.i_, rhs) == 0 )return true;
+                return false;
+                // return (lhs.i_ == rhs);
             }
             // == int
-            friend bool    operator==(const FiniteFieldElement<P>& lhs, int rhs)
+            friend bool    operator==(const FiniteFieldElement& lhs, int rhs)
             {
-                return (lhs.i_ == rhs);
+                BIGNUM* tmp = BN_new();
+                BN_set_word(tmp, rhs); 
+                if( BN_cmp(lhs.i_, tmp) == 0 )return true;
+                return false;
+                // return (lhs.i_ == rhs);
             }
-            // !=
-            friend bool    operator!=(const FiniteFieldElement<P>& lhs, int rhs)
+
+            // != Field
+            friend bool    operator!=(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
             {
-                return (lhs.i_ != rhs);
-            }                
+                if( BN_cmp(lhs.i_, rhs.i_) == 0 )return false;
+                return true;
+                // return (lhs.i_ != rhs.i_);
+            }
+            // != BIGNUM
+            friend bool    operator!=(const FiniteFieldElement& lhs, BIGNUM* rhs)
+            {   
+                if( BN_cmp(lhs.i_, rhs) == 0 )return false;
+                return true;
+                // return (lhs.i_ != rhs);
+            }
+            // != int
+            friend bool    operator!=(const FiniteFieldElement& lhs, int rhs)
+            {   
+                BIGNUM* tmp = BN_new();
+                BN_set_word(tmp, rhs); 
+                if( BN_cmp(lhs.i_, tmp) == 0 )return false;
+                return true;
+                // return (lhs.i_ != rhs);
+            }
+
             // a / b
-            friend FiniteFieldElement<P> operator/(const FiniteFieldElement<P>& lhs, const FiniteFieldElement<P>& rhs)
+            friend FiniteFieldElement operator/(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,P));
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, lhs.i_, helper::MultiplicativeInverse(rhs.i_,PRIME), ctx);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,PRIME));
             }
-            // a + b
-            friend FiniteFieldElement<P> operator+(const FiniteFieldElement<P>& lhs, const FiniteFieldElement<P>& rhs)
+            // a % b
+            friend FiniteFieldElement operator%(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( lhs.i_ + rhs.i_);
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mod(tmp, lhs.i_, rhs.i_, ctx);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,PRIME));
+            }
+            // BIGNUM % b
+            friend FiniteFieldElement operator%(BIGNUM* lhs, const FiniteFieldElement& rhs)
+            {
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mod(tmp, lhs, rhs.i_, ctx);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,PRIME));
+            }
+            // b % BIGNUM
+            friend FiniteFieldElement operator%(const FiniteFieldElement& lhs, BIGNUM* rhs)
+            {
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mod(tmp, lhs.i_, rhs, ctx);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,PRIME));
+            }
+            // int % b
+            friend FiniteFieldElement operator%(int n, const FiniteFieldElement& rhs)
+            {
+                BIGNUM *lhs = BN_new();
+                BN_set_word(lhs, n);
+
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mod(tmp, lhs, rhs.i_, ctx);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ * helper::MultiplicativeInverse(rhs.i_,PRIME));
+            }
+            
+
+            // a + b
+            friend FiniteFieldElement operator+(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
+            {
+                BIGNUM* tmp = BN_new();
+                BN_add(tmp, lhs.i_, rhs.i_);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ + rhs.i_);
             }
             // a - b
-            friend FiniteFieldElement<P> operator-(const FiniteFieldElement<P>& lhs, const FiniteFieldElement<P>& rhs)
+            friend FiniteFieldElement operator-(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( lhs.i_ - rhs.i_);
+                BIGNUM* tmp = BN_new();
+                BN_sub(tmp, lhs.i_, rhs.i_);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_ - rhs.i_);
+            }
+            // a + BIGNUM
+            friend FiniteFieldElement operator+(const FiniteFieldElement& lhs, BIGNUM* i)
+            {
+                BIGNUM* tmp = BN_new();
+                BN_add(tmp, lhs.i_, i);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_+i);
+            }
+            // BIGNUM + a
+            friend FiniteFieldElement operator+(BIGNUM* i, const FiniteFieldElement& rhs)
+            {
+                BIGNUM* tmp = BN_new();
+                BN_add(tmp, i, rhs.i_);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( rhs.i_+i);
             }
             // a + int
-            friend FiniteFieldElement<P> operator+(const FiniteFieldElement<P>& lhs, int i)
+            friend FiniteFieldElement operator+(const FiniteFieldElement& lhs, int n)
             {
-                return FiniteFieldElement<P>( lhs.i_+i);
+                BIGNUM* tmp2 = BN_new();
+                BN_set_word(tmp2, n); 
+
+                BIGNUM* tmp = BN_new();
+                BN_add(tmp, lhs.i_, tmp2);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( lhs.i_+i);
             }
             // int + a
-            friend FiniteFieldElement<P> operator+(int i, const FiniteFieldElement<P>& rhs)
+            friend FiniteFieldElement operator+(int n, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( rhs.i_+i);
+                BIGNUM* tmp2 = BN_new();
+                BN_set_word(tmp2, n); 
+
+                BIGNUM* tmp = BN_new();
+                BN_add(tmp, tmp2, rhs.i_);
+                return FiniteFieldElement(tmp);
+                // return FiniteFieldElement( rhs.i_+i);
+            }
+
+            // BIGNUM * a
+            friend FiniteFieldElement operator*(BIGNUM* n, const FiniteFieldElement& rhs)
+            {
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, n, rhs.i_, ctx);
+                return FiniteFieldElement( tmp );
+            }
+            // a * BIGNUM
+            friend FiniteFieldElement operator*(const FiniteFieldElement& lhs, BIGNUM* n)
+            {
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, n, lhs.i_, ctx);
+                return FiniteFieldElement( tmp );
             }
             // int * a
-            friend FiniteFieldElement<P> operator*(int n, const FiniteFieldElement<P>& rhs)
+            friend FiniteFieldElement operator*(int n, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( n*rhs.i_);
-            }                
+                BIGNUM* tmp2 = BN_new();
+                BN_set_word(tmp2, n); 
+
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, tmp2, rhs.i_, ctx);
+                return FiniteFieldElement( tmp );
+            }
+            // a * int
+            friend FiniteFieldElement operator*(const FiniteFieldElement& lhs, int n)
+            {
+                BIGNUM* tmp2 = BN_new();
+                BN_set_word(tmp2, n); 
+
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, lhs.i_, tmp2, ctx);
+                return FiniteFieldElement( tmp );
+            }
+            // // BIGNUM * int
+            // friend FiniteFieldElement operator*(BIGNUM* lhs, int n)
+            // {
+            //     BIGNUM* tmp2 = BN_new();
+            //     BN_set_word(tmp2, n); 
+
+            //     BN_CTX *ctx = BN_CTX_new();
+            //     BIGNUM* tmp = BN_new();
+            //     BN_mul(tmp, lhs, tmp2, ctx);
+            //     return FiniteFieldElement( tmp );
+            // }
+            // // int * BIGNUM
+            // friend FiniteFieldElement operator*(int n, BIGNUM* rhs)
+            // {
+            //     BIGNUM* tmp2 = BN_new();
+            //     BN_set_word(tmp2, n); 
+
+            //     BN_CTX *ctx = BN_CTX_new();
+            //     BIGNUM* tmp = BN_new();
+            //     BN_mul(tmp, tmp2, rhs, ctx);
+            //     return FiniteFieldElement( tmp );
+            // }
+
             // a * b
-            friend FiniteFieldElement<P> operator*(const FiniteFieldElement<P>& lhs, const FiniteFieldElement<P>& rhs)
+            friend FiniteFieldElement operator*(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs)
             {
-                return FiniteFieldElement<P>( lhs.i_ * rhs.i_);
+                BN_CTX *ctx = BN_CTX_new();
+                BIGNUM* tmp = BN_new();
+                BN_mul(tmp, lhs.i_, rhs.i_, ctx);
+                return FiniteFieldElement( tmp );
             }
             // ostream handler
-            template<int T>
-            friend  ostream&    operator<<(ostream& os, const FiniteFieldElement<T>& g)
+            // template<BIGNUM* T>
+            friend  std::ostream&    operator<<(std::ostream& os, const FiniteFieldElement& g)
             {
-                return os << g.i_;
+                string ans = BN_bn2dec(g.i_);
+                return os << ans;
             }
     };
 }
 
-    
