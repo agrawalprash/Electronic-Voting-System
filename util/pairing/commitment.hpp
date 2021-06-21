4ebb3c6ead7573658ab2e4cef3628040a74ab91d
Prakhar Aggarwal <cs5190441@cse.iitd.ac.in>
2021-06-21 11:11:35 +0530

    pairing with testing

---
+++ util/pairing/commitment.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include "pair.hpp"
+
+void commit(element_t Commitment, element_t g, element_t Message, element_t h, element_t random);
+
+void commit(element_t Commitment, element_t g, mpz_t Message, element_t h, mpz_t random);
+
+void commit(element_t Commitment, mpz_t msg, mpz_t r, Pairing* pairing);
+
+void commit(element_t Commitment, string msg, mpz_t r, Pairing* pairing);
+
+void commit(element_t Commitment, string msg, string randomness, Pairing* pairing);
+
+bool open(element_t Commitment, element_t g, element_t Message, element_t h, element_t random);
+
+bool open(element_t Commitment, element_t g, mpz_t Message, element_t h, mpz_t random);
+
+
+// Class for Commitment, Setup, commit, open
+// virtual functions <- modularize
+// UML diagrams
+// Class diagrams
+// activity, behaviour diagrams
+// How these diagrams can be used to stitch together
